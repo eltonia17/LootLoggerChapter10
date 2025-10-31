fb7ee48549bc09434fd097a93fc25ebd2d467d4c
Leonard <esl107@miami.edu>
2025-10-31 13:40:14 -0400

    Initial Commit

---
+++ LootLoggerTests/LootLoggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LootLoggerTests.swift
+//  LootLoggerTests
+//
+//  Created by Eltonia Leonard on 10/31/25.
+//
+
+import Testing
+@testable import LootLogger
+
+struct LootLoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
