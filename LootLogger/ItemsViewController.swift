788bc5635a6375a947197b2b86eddbedb3a86d8f
Leonard <esl107@miami.edu>
2025-10-31 18:13:34 -0400

    Using itemCell

--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -51,16 +51,18 @@ class ItemsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView,
             cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Get a new or recycled cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",
-            for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell",
+                                                     for: indexPath) as! ItemCell
 
         // Set the text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
         // will appear in on the table view
         let item = itemStore.allItems[indexPath.row]
 
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        // Configure the cell with the Item
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
 
         return cell
     }
@@ -87,5 +89,10 @@ class ItemsViewController: UITableViewController {
         itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.rowHeight = 65
+    }
 }
 
